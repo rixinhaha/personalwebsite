8b6b2572dde4d843405a1794b7d130b0acf3cef7
Rex <rixin@ymail.com>
2019-12-20 20:58:32 -0800

    activate featured imaged

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "http://rixinhaha.com/",
+        baseUrl: "http://c97ae4e0.ngrok.io/",
         // The protocol. This can be http or https.
         protocol: "http",
         // Indicates whether the site is hosted on wordpress.com.
